4f913c2fd291c1f7c53deffa295850a512030d7c
Simon Dupree <dupreenator@gmail.com>
2018-07-12 11:33:33 +0100

    logic and  test basics completed

--- logic.js
+++ logic.js
@@ -2,7 +2,6 @@
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
 
-
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -19,28 +18,28 @@ var todoFunctions = {
   //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
-    return todos.map(function(todo){
+    return todos.map(function(todo) {
       return JSON.parse(JSON.stringify(todo));
     });
   },
 
   addTodo: function(todos, newTodo) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // returns a new array, it should contain todos with the newTodo added to the end.
-    // add an id to the newTodo. You can use the generateId function to create an id.
-    // hint: array.concat
-
-  todos.concat(newTodo);
-  return todos;
-
+    console.log(typeof newTodo);
+    console.log(Array.isArray(todos));
+    var arr = [];
+    arr.push(newTodo);
+    todos = todos.concat(arr);
+    return todos;
   },
+
   deleteTodo: function(todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
     var newArr = this.cloneArrayOfObjects(todos);
     newArr = newArr.filter(newArrElem => newArrElem.id != idToDelete);
-    return newArr;
+    todos = newArr;
+    return todos;
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
@@ -48,31 +47,30 @@ var todoFunctions = {
     // this element will have its done value toggled
     // hint: array.map
     var newArr = this.cloneArrayOfObjects(todos);
-    newArr.map(function(val){
-      if(val.id == idToMark)
-      {
-        if(val.done == true)
-        { val.done = false; }
-        else
-        {val.done = true; }
+    newArr.map(function(val) {
+      if (val.id == idToMark) {
+        if (val.done == true) {
+          val.done = false;
+        } else {
+          val.done = true;
+        }
       }
     });
-    return newArr;
-
+    todos = newArr;
+    return todos;
   },
   sortTodos: function(todos, sortFunction) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
     // hint: array.slice, array.sort
-  },
+  }
 };
-
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
 // See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = todoFunctions;
 }
