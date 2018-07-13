250ad2b33e5e4bfbb86883b63bef580854a577e3
Simon Dupree <dupreenator@gmail.com>
2018-07-13 11:58:08 +0100

    code reviewed and tape test added, nodemon set up

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
 
+// thats the overall todoFunctions-Object
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -23,6 +24,7 @@ var todoFunctions = {
     });
   },
 
+  // addTodo adds a new element to the end of the todos array of objects
   addTodo: function(todos, newTodo) {
     var arr = [];
     arr.push(newTodo);
@@ -30,26 +32,21 @@ var todoFunctions = {
     return todos;
   },
 
+  // deleteTodo removes a element of the array of objects
   deleteTodo: function(todos, idToDelete) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // return a new array, this should not contain any todo with an id of idToDelete
-    // hint: array.filter
     var newArr = this.cloneArrayOfObjects(todos);
     newArr = newArr.filter(newArrElem => newArrElem.id != idToDelete);
     todos = newArr;
     return todos;
   },
+
+  // markTodo creates a toggle function to either mark or unmark todo-tasks
   markTodo: function(todos, idToMark) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
-    // this element will have its done value toggled
-    // hint: array.map
     var newArr = this.cloneArrayOfObjects(todos);
     newArr.map(function(val) {
       if (val.id == idToMark) {
         if (val.done == true) {
           val.done = false;
-
         } else {
           val.done = true;
         }
@@ -58,18 +55,22 @@ var todoFunctions = {
     todos = newArr;
     return todos;
   },
-  sortTodos: function(todos, sortFunction) {
-    // stretch goal! Do this last
-    // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
-    // sortFunction will have same signature as the sort function in array.sort
-    // hint: array.slice, array.sort
+
+  // sortTodos sorts the elements according to their done status (true or false)
+  sortTodos: function(todos) {
+    var newArr = this.cloneArrayOfObjects(todos);
+    newArr.sort(function(firstTask, secondTask) {
+      if (firstTask.done === true) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    todos = newArr;
+    return todos;
   }
 };
 
-// Why is this if statement necessary?
-// The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details:
-// http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== "undefined") {
   module.exports = todoFunctions;
 }
